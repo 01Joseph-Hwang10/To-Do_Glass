0c9afc0ee4b65cbb606584a496e3514e333ea98e
01Joseph-Hwang10 <joseph95501@gmail.com>
2021-02-14 11:12:47 +0900

    working on description figure

--- src/routes/Landing.js
+++ src/routes/Landing.js
@@ -10,6 +10,8 @@ import "../static/css/Landing.css";
 import { COLOR_FIRST, COLOR_FOURTH } from "../store/variables";
 import bg5 from '../static/images/bg5.jpg'
 // import bg2 from '../static/images/bg2.jpg'
+// Components
+import Important from "../mixins/Important";
 
 function Landing(props) {
 
@@ -53,62 +55,74 @@ function Landing(props) {
                     <div className="mt-32 mb-32"></div>
                 </div>
                 
-                <div className="my-16 w-full"></div>
+                <div className="my-20 w-full"></div>
 
-                <div className="containerFirst w-full flex justify-around items-center">
-                    <div className="descriptionFirst flex flex-col justify-center items-start space-y-2">
-                        <div className="flex flex-col justify-center items-start"><span className="text-3xl font-semibold">Start the project</span></div>
-                        <div className="flex flex-col justify-center items-start">
-                            <span className="text-gray-700">Flglance provides the powerful tools</span>
-                            <span className="text-gray-700">For your project planning and workflow managing</span>
-                            <span className="text-gray-700">Just name and create, and begin the journey</span>
+                <div className='description w-10/12 2xl:w-8/12'>
+                    <div className="containerFirst w-full flex justify-around items-center">
+                        <div className="descriptionFirst flex flex-col justify-center items-start space-y-2">
+                            <div className="flex flex-col justify-center items-start"><span className="text-3xl font-semibold">Start the project</span></div>
+                            <div className="flex flex-col justify-center items-start">
+                                <span className="text-gray-700">Flglance provides the powerful tools</span>
+                                <span className="text-gray-700">For your project planning and workflow managing</span>
+                                <span className="text-gray-700">Just name and create, and begin the journey</span>
+                            </div>
+                        </div>
+                        <div className="figureFirst flex justify-center items-start space-x-2">
+                            <div className="flex flex-col justify-start items-center">
+                                <div className="w-full p-1 px-2 mt-2 rounded flex justify-between" style={{backgroundColor:COLOR_FOURTH}}>
+                                    <div><span className="xl:text-lg font-semibold underline text-blue-600">My First Project!!</span></div>
+                                    <div><Important isImportant={false} /></div>
+                                </div>
+                            </div>
+                            <div className="h-64 w-48 flex justify-center items-center rounded p-2" style={{backgroundColor:COLOR_FOURTH}}>
+                                <span>No Projects Are Opened</span>
+                            </div>
                         </div>
                     </div>
-                    <div className="figureFirst"></div>
-                </div>
 
-                <div className="my-10 w-full"></div>
+                    <div className="my-16 w-full"></div>
 
-                <div className="containerSecond w-full flex justify-around items-center">
-                    <div className="figureSecond"></div>
-                    <div className="descriptionSecond flex flex-col justify-center items-start space-y-2">
-                        <div className="flex flex-col justify-center items-start"><span className="text-3xl font-semibold">Construct the Scheme</span></div>
-                        <div className="flex flex-col justify-center items-start">
-                            <span className="text-gray-700">Set up the frame of the project</span>
-                            <span className="text-gray-700">Inspired by PERT chart, it gives you the big picture of project</span>
-                            <span className="text-gray-700">Again, Just name and create, and you're right to go</span>
+                    <div className="containerSecond w-full flex justify-around items-center">
+                        <div className="figureSecond"></div>
+                        <div className="descriptionSecond flex flex-col justify-center items-start space-y-2">
+                            <div className="flex flex-col justify-center items-start"><span className="text-3xl font-semibold">Construct the Scheme</span></div>
+                            <div className="flex flex-col justify-center items-start">
+                                <span className="text-gray-700">Set up the frame of the project</span>
+                                <span className="text-gray-700">Inspired by PERT chart, it gives you the big picture of project</span>
+                                <span className="text-gray-700">Again, Just name and create, and you're right to go</span>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div className="my-10 w-full"></div>
+                    <div className="my-16 w-full"></div>
 
-                <div className="containerThird w-full flex justify-around items-center">
-                    <div className="descriptionThird flex flex-col justify-center items-start space-y-2">
-                        <div className="flex flex-col justify-center items-start"><span className="text-3xl font-semibold">Add the Task</span></div>
-                        <div className="flex flex-col justify-center items-start">
-                            <span className="text-gray-700">Make it more concrete, more vivid</span>
-                            <span className="text-gray-700">Inspired by GANTT chart, it ables you to manage tasksefficiently</span>
-                            <span className="text-gray-700">You can also move your tasks like a Post-it,</span>
-                            <span className="text-gray-700">Makes the management more efficiently</span>
+                    <div className="containerThird w-full flex justify-around items-center">
+                        <div className="descriptionThird flex flex-col justify-center items-start space-y-2">
+                            <div className="flex flex-col justify-center items-start"><span className="text-3xl font-semibold">Add the Task</span></div>
+                            <div className="flex flex-col justify-center items-start">
+                                <span className="text-gray-700">Make it more concrete, more vivid</span>
+                                <span className="text-gray-700">Inspired by GANTT chart, it ables you to manage tasksefficiently</span>
+                                <span className="text-gray-700">You can also move your tasks like a Post-it,</span>
+                                <span className="text-gray-700">Makes the management more efficiently</span>
+                            </div>
+                        </div>
+                        <div className="figureThird"></div>
+                    </div>
+
+                    <div className="my-16 w-full"></div>
+
+                    <div className="containerFourth w-full flex justify-around items-center">
+                        <div className="figureFourth"></div>
+                        <div className="descriptionFourth flex flex-col justify-center items-start space-y-2">
+                            <div className="flex flex-col justify-center items-start"><span className="text-3xl font-semibold">You're not Alone</span></div>
+                            <div className="flex flex-col justify-center items-start">
+                                <span className="text-gray-700">Glance over the other's workflow</span>
+                                <span className="text-gray-700">And give your project another rize</span>
+                                <span className="text-gray-700">Follow who you're interested in</span>
+                                <span className="text-gray-700">And get his/her workflow firstly</span>
+                            </div>
                         </div>
                     </div>
-                    <div className="figureThird"></div>
-                </div>
-
-                <div className="my-10 w-full"></div>
-
-                <div className="containerFourth w-full flex justify-around items-center">
-                    <div className="descriptionFourth flex flex-col justify-center items-start space-y-2">
-                        <div className="flex flex-col justify-center items-start"><span className="text-3xl font-semibold">You're not Alone</span></div>
-                        <div className="flex flex-col justify-center items-start">
-                            <span className="text-gray-700">Glance over the other's workflow</span>
-                            <span className="text-gray-700">And give your project another rize</span>
-                            <span className="text-gray-700">Follow who you're interested in</span>
-                            <span className="text-gray-700">And get his/her workflow firstly</span>
-                        </div>
-                    </div>
-                    <div className="figureFourth"></div>
                 </div>
 
             </div>
