e627622f3e19a28c4c21b8e7f724b3bd97c2bd41
01Joseph-Hwang10 <joseph95501@gmail.com>
2021-02-19 03:11:56 +0900

    needa solve proxy issue

--- frontend/src/store/variables.js
+++ frontend/src/store/variables.js
@@ -1,27 +1,27 @@
 // Base url
 export const URL_PROXY = process.env.BACKEND_API;
 // Auth url
-export const URL_SIGN_UP = `${URL_PROXY}/users-api/sign-up/`;
-export const URL_TOKEN = `${URL_PROXY}/users-api/token/`;
-export const URL_TOKEN_REFRESH=`${URL_PROXY}/users-api/token/refresh/`;
-export const URL_CHECK_SELF_AUTH=`${URL_PROXY}/users-api/check-self-auth/`;
-export const URL_LOGOUT=`${URL_PROXY}/users-api/logout/`;
+export const URL_SIGN_UP = `${URL_PROXY}/api/users-api/sign-up/`;
+export const URL_TOKEN = `${URL_PROXY}/api/users-api/token/`;
+export const URL_TOKEN_REFRESH=`${URL_PROXY}/api/users-api/token/refresh/`;
+export const URL_CHECK_SELF_AUTH=`${URL_PROXY}/api/users-api/check-self-auth/`;
+export const URL_LOGOUT=`${URL_PROXY}/api/users-api/logout/`;
 // Profile url
-export const URL_PUBLIC_PROFILE=`${URL_PROXY}/users-api/public_users/`;
-export const URL_PROFILE=`${URL_PROXY}/users-api/users/`;
+export const URL_PUBLIC_PROFILE=`${URL_PROXY}/api/users-api/public_users/`;
+export const URL_PROFILE=`${URL_PROXY}/api/users-api/users/`;
 // Project url
-export const URL_PUBLIC_PROJECT=`${URL_PROXY}/todos-api/public_projects/`;
-export const URL_PROJECT=`${URL_PROXY}/todos-api/projects/`;
-export const URL_GLANCE=`${URL_PROXY}/todos-api/glance/`;
+export const URL_PUBLIC_PROJECT=`${URL_PROXY}/api/todos-api/public_projects/`;
+export const URL_PROJECT=`${URL_PROXY}/api/todos-api/projects/`;
+export const URL_GLANCE=`${URL_PROXY}/api/todos-api/glance/`;
 // Tag url
-export const URL_PUBLIC_TAG=`${URL_PROXY}/todos-api/public_tags/`;
-export const URL_TAG=`${URL_PROXY}/todos-api/tags/`;
+export const URL_PUBLIC_TAG=`${URL_PROXY}/api/todos-api/public_tags/`;
+export const URL_TAG=`${URL_PROXY}/api/todos-api/tags/`;
 // Container url
-export const URL_PUBLIC_CONTAINER=`${URL_PROXY}/todos-api/public_containers/`;
-export const URL_CONTAINER = `${URL_PROXY}/todos-api/containers/`;
+export const URL_PUBLIC_CONTAINER=`${URL_PROXY}/api/todos-api/public_containers/`;
+export const URL_CONTAINER = `${URL_PROXY}/api/todos-api/containers/`;
 // Task url
-export const URL_PUBLIC_TASK=`${URL_PROXY}/todos-api/public_tasks/`;
-export const URL_TASK=`${URL_PROXY}/todos-api/tasks/`;
+export const URL_PUBLIC_TASK=`${URL_PROXY}/api/todos-api/public_tasks/`;
+export const URL_TASK=`${URL_PROXY}/api/todos-api/tasks/`;
 
 // Colors
 // Set 1
