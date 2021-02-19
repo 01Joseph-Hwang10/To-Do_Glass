9d4deebce8c4de12cd24fa49671bd9af0ad58411
01Joseph-Hwang10 <joseph95501@gmail.com>
2021-02-19 23:42:32 +0900

    sentry added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,6 +14,19 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://0fadfdb1ef064f95b3efc24a3b94651c@o496976.ingest.sentry.io/5644483",
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
