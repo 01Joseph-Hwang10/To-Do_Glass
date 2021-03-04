63eefd05c8bb01e8cde5f57e7f2cd799044b33d2
01Joseph-Hwang10 <joseph95501@gmail.com>
2021-03-04 21:25:07 +0900

    secure option added

--- backend/users/views.py
+++ backend/users/views.py
@@ -35,9 +35,9 @@ class CustomTokenObtainPairView(TokenObtainPairView):
         max_age_54weeks = 54*7*24*60*60
 
         # Need to add Secure options!!
-        auth_response.set_cookie("access_token",value=serializer.validated_data['access'],max_age=max_age_1day,httponly=True)
-        auth_response.set_cookie("refresh_token",value=serializer.validated_data['refresh'],max_age=max_age_54weeks,httponly=True)
-        auth_response.set_cookie("user_id",value=user_id,max_age=max_age_54weeks,httponly=True)
+        auth_response.set_cookie("access_token",value=serializer.validated_data['access'],max_age=max_age_1day,secure=True,httponly=True)
+        auth_response.set_cookie("refresh_token",value=serializer.validated_data['refresh'],max_age=max_age_54weeks,secure=True,httponly=True)
+        auth_response.set_cookie("user_id",value=user_id,max_age=max_age_54weeks,secure=True,httponly=True)
 
         return auth_response
 
@@ -61,7 +61,7 @@ class CustomTokenRefreshView(TokenRefreshView):
         max_age_1day = 24*60*60
 
         # Need to add Secure options!!
-        auth_response.set_cookie("access_token",value=serializer.validated_data['access'],max_age=max_age_1day,httponly=True)
+        auth_response.set_cookie("access_token",value=serializer.validated_data['access'],max_age=max_age_1day,secure=True,httponly=True)
 
         return auth_response
 
